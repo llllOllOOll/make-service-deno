3da1f374257a2ac1a7aa5aa68a63bd9520eff876
llllOllOOll <7b37b3@gmail.com>
2023-07-09 22:42:18 -0300

    src folder created and add constants - internals - types from make-service officiela

---
+++ src/internals.ts
@@ -0,0 +1,54 @@
+import { Schema } from "./types";
+
+/**
+ * It returns the JSON object or throws an error if the response is not ok.
+ * @param response the Response to be parsed
+ * @returns the response.json method that accepts a type or Zod schema for a typed json response
+ */
+function getJson(response: Response) {
+  return async <T = unknown>(schema?: Schema<T>): Promise<T> => {
+    const json = await response.json();
+    return schema ? schema.parse(json) : (json as T);
+  };
+}
+
+/**
+ * @param response the Response to be parsed
+ * @returns the response.text method that accepts a type or Zod schema for a typed response
+ */
+function getText(response: Response) {
+  return async <T extends string = string>(schema?: Schema<T>): Promise<T> => {
+    const text = await response.text();
+    return schema ? schema.parse(text) : (text as T);
+  };
+}
+
+/**
+ * This is an enhanced version of the typeof operator to check the type of more complex values.
+ * @param t the value to be checked
+ * @returns the type of the value
+ */
+function typeOf(t: unknown) {
+  return Object.prototype.toString
+    .call(t)
+    .replace(/^\[object (.+)\]$/, "$1")
+    .toLowerCase() as
+    | "array"
+    | "arraybuffer"
+    | "bigint"
+    | "blob"
+    | "boolean"
+    | "formdata"
+    | "function"
+    | "null"
+    | "number"
+    | "object"
+    | "readablestream"
+    | "string"
+    | "symbol"
+    | "undefined"
+    | "url"
+    | "urlsearchparams";
+}
+
+export { getJson, getText, typeOf };
